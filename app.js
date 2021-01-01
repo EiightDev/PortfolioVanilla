8ef96aad7834708e8d642c04388c5887c1b71753
Mustapha <eiightdev@gmail.com>
2021-01-01 17:29:41 +0100

    feat new skill

--- app.js
+++ app.js
@@ -32,28 +32,13 @@ typewriter
   .typeString("<strong style='color: black;'> Full-Stack</strong> !")
   .start();
 
-// Animation Contact
-
-const input_fields = document.querySelectorAll("input");
-
-for (let i = 0; i < input_fields.length; i++) {
-  let field = input_fields[i];
-
-  field.addEventListener("input", (e) => {
-    if (e.target.value !== "") {
-      e.target.parentNode.classList.add("animation");
-    } else if (e.target.value == "") {
-      e.target.parentNode.classList.remove("animation");
-    }
-  });
-}
 
 // Animation ScrollMagic GSAP presentation
 
 const presentationContainer = document.querySelector(".presentation");
 const titrePres = document.querySelector(".titre-pres");
 const presTxt = document.querySelector(".pres-txt");
-const listePres = document.querySelectorAll(".item-liste");
+const listePres = document.querySelectorAll(".item-list");
 
 const tlpres = new TimelineMax();
 
@@ -71,6 +56,7 @@ const scene = new ScrollMagic.Scene({
 })
   .setTween(tlpres)
   .addTo(controller);
+
 
 // Anim portfolio
 
@@ -92,13 +78,35 @@ const scene2 = new ScrollMagic.Scene({
   .setTween(tlPortfolio)
   .addTo(controller);
 
+// const skillContainer = document.querySelector(".skill-container");
+// const titreSkill = document.querySelector(".titre-skill");
+// const itemSkill = document.querySelectorAll(".panneau");
+
+// const tlSkill = new TimelineMax();
+
+// tlSkill
+//   .from(titreSkill, { y: -50, opacity: 0, duration: 0.5 })
+//   .staggerFrom(itemPortfolio, 1, { opacity: 0 }, 0.2, "-=0.5");
+
+// const scene2 = new ScrollMagic.Scene({
+//   triggerElement: portfolioContainer,
+//   triggerHook: 0.5,
+//   reverse: false,
+// })
+//   .setTween(tlSkill)
+//   .addTo(controller);
+
+
+
+
+
+
 
 // Animation Panneau
 const allBlocs = document.querySelectorAll(".panneau");
 
 allBlocs.forEach((bloc) => {
   bloc.addEventListener("click", (e) => {
-    console.log(e.target);
     e.target.classList.add("grow");
 
     for (let i = 0; i < allBlocs.length; i++) {
@@ -110,7 +118,6 @@ allBlocs.forEach((bloc) => {
 });
 const allRonds = document.querySelectorAll(".rond-exp");
 const allBoxes = document.querySelectorAll(".box");
-
 
 allBoxes.forEach((box) => {
   for (i = 0; i < allRonds.length; i++) {
@@ -128,3 +135,24 @@ allBoxes.forEach((box) => {
     }
   }
 });
+
+
+
+
+
+
+// Animation Contact pour les labels
+
+const input_fields = document.querySelectorAll("input");
+
+for (let i = 0; i < input_fields.length; i++) {
+  let field = input_fields[i];
+
+  field.addEventListener("input", (e) => {
+    if (e.target.value !== "") {
+      e.target.parentNode.classList.add("animation");
+    } else if (e.target.value == "") {
+      e.target.parentNode.classList.remove("animation");
+    }
+  });
+}
